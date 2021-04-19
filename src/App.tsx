aed3b9acfd6ce93f208d0d8d21f2ff7869af28d6
luchosr <luchosr@gmail.com>
2021-04-19 10:02:55 +0200

    add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
+import api from "./api";
+import {Joke , Status} from "./types";
+import "./styles.css";
+
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [jokes, setJokes] = React.useState<Joke[]>([]);
+  const [status, setStatus] = React.useState<Status>(Status.Init);
+
+  function getJoke() {
+    setStatus(Status.Pending);
+
+    api
+      .fetch()
+      .then((joke) => {
+        setJokes((jokes) => jokes.concat(joke));
+        setStatus(Status.Resolved);
+      })
+      .catch(() => setStatus(Status.Rejected));
+  }
+
   return (
     <div className="App">
-      
+      <h1>Chuck Norris Jokes</h1>
+      {jokes.length ? (
+        <ul>
+          {jokes.map((joke) => (
+            <li key={joke.id}>{joke.value}</li>
+          ))}
+        </ul>
+      ) : (
+        <span>No jokes yet</span>
+      )}
+      <button disabled={status === Status.Pending} onClick={getJoke}>
+        Fetch joke
+      </button>
     </div>
   );
-}
+};
 
 export default App;
